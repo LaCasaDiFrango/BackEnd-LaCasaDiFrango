bd25cd60045574327410636e07a220ba7fecbdc8
HenriqueChepli <henriquechepli@gmail.com>
2025-07-01 14:02:05 -0300

    feat: inclusão do total da compra na listagem de compras #21

--- core/models/pedido/item_pedido.py
+++ core/models/pedido/item_pedido.py
@@ -8,6 +8,13 @@ class ItemPedido(models.Model):
     produto = models.ForeignKey('Produto', on_delete=models.PROTECT, verbose_name='Produto')
     quantidade = models.PositiveIntegerField(verbose_name='Quantidade', default=1)
 
+    @property
+    def total(self):
+        # total = 0
+        # for item in self.itens.all():
+        #     total += item.produto.preco * item.quantidade
+        # return total
+        return sum(item.produto.preco * item.quantidade for item in self.itens.all())
 
 
     def __str__(self):
--- core/serializers/pedido/pedido.py
+++ core/serializers/pedido/pedido.py
@@ -1,10 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.models.pedido.pedido import Pedido
+from core.models.pedido.item_pedido import ItemPedido
 from core.serializers.pedido.item_pedido import ItemPedidoSerializer
 
 class PedidoSerializer(ModelSerializer):
     class Meta:
         model = Pedido
-        fields = '__all__'
+        fields = ('id', 'usuario', 'status', 'total', 'itens')
         itens = ItemPedidoSerializer(many=True, read_only=True)
