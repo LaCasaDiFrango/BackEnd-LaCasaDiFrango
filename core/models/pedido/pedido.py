7f7ca629bbd7ba212d0ade69ee137e89458113e2
HenriqueChepli <henriquechepli@gmail.com>
2025-07-01 09:00:09 -0300

    feat: creation of the Purchase entity integrated with the project’s user #18

--- core/models/pedido/pedido.py
+++ core/models/pedido/pedido.py
@@ -3,9 +3,15 @@ from django.db import models
 from core.models.usuario.user import User
 
 class Pedido(models.Model):
+    class StatusCompra(models.IntegerChoices):
+        CARRINHO = 1, "Carrinho"
+        FINALIZADO = 2, "Realizado"
+        PAGO = 3, "Pago"
+        ENTREGUE = 4, "Entregue"
+
     preco_total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço Total do Pedido', default=0.00)
     data_de_retirada = models.DateTimeField(verbose_name='Data de Validade do Pedido', auto_now_add=True)
-    status = models.CharField(max_length=20, verbose_name='Status do Pedido', default='Pendente')
+    status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Usuário do Pedido', null=True, blank=True)
 
 
