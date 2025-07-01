5bf5d928913028deb3406ff16a764c0d5f3c8416
HenriqueChepli <henriquechepli@gmail.com>
2025-07-01 13:57:03 -0300

    feat: mostrando o total do item na listagem de compras #21

--- core/serializers/pedido/item_pedido.py
+++ core/serializers/pedido/item_pedido.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.models.pedido.item_pedido import ItemPedido
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
 class ItemPedidoSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.produto.preco * instance.quantidade
+
     class Meta:
         model = ItemPedido
-        fields = ('produto', 'quantidade')
-        depth: 1
+        fields = ('produto', 'quantidade', 'total')
+        depth = 1
