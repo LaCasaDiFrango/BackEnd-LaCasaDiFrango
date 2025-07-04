b413c70d8a283f440fb53d05a8ed2a0916aad30c
HenriqueChepli <henriquechepli@gmail.com>
2025-07-04 12:57:17 -0300

    feat: validando a quantidade de itens na compra #25

--- core/serializers/pedido/item_pedido.py
+++ core/serializers/pedido/item_pedido.py
@@ -1,7 +1,6 @@
-from rest_framework.serializers import ModelSerializer
 
 from core.models.pedido.item_pedido import ItemPedido
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField, ValidationError
 
 class ItemPedidoSerializer(ModelSerializer):
     total = SerializerMethodField()
@@ -18,6 +17,16 @@ class ItemPedidoCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItemPedido
         fields = ('produto', 'quantidade')
+
+    def validate_quantidade(self, quantidade):
+        if quantidade <= 0:
+            raise ValidationError('A quantidade deve ser maior do que zero.')
+        return quantidade
+
+    def validate(self, item):
+        if item['quantidade'] > item['produto'].quantidade:
+            raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
+        return item
 
 class ItemPedidoListSerializer(ModelSerializer):
     produto = CharField(source='produto.nome', read_only=True)
