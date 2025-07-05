d89eac6802e12750edca85af6a352eed3d36a38f
HenriqueChepli <henriquechepli@gmail.com>
2025-07-04 23:01:25 -0300

    feat: alterando o preço de um produto #27

--- core/serializers/produto/produto.py
+++ core/serializers/produto/produto.py
@@ -17,3 +17,12 @@ class ProdutoRetrieveSerializer(ModelSerializer):
         model = Produto
         fields = "__all__"
         depth = 1
+
+class ProdutoAlterarPrecoSerializer(Serializer):
+    preco = DecimalField(max_digits=10, decimal_places=2)
+
+    def validate_preco(self, value):
+        '''Valida se o preço é um valor positivo.'''
+        if value <= 0:
+            raise ValidationError('O preço deve ser um valor positivo.')
+        return value
--- core/views/produto/produto.py
+++ core/views/produto/produto.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models.produto.produto import Produto
-from core.serializers.produto.produto import ProdutoSerializer, ProdutoListSerializer, ProdutoRetrieveSerializer
+from core.serializers.produto.produto import ProdutoSerializer, ProdutoListSerializer, ProdutoRetrieveSerializer, ProdutoAlterarPrecoSerializer
 
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
@@ -11,3 +11,17 @@ class ProdutoViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return ProdutoRetrieveSerializer
         return ProdutoSerializer
+
+    @action(detail=True, methods=['patch'])
+    def alterar_preco(self, request, pk=None):
+        produto = self.get_object()
+
+        serializer = ProdutoAlterarPrecoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        produto.preco = serializer.validated_data['preco']
+        produto.save()
+
+        return Response(
+            {'detail': f'Preço do produto "{produto.titulo}" atualizado para {produto.preco}.'}, status=status.HTTP_200_OK
+        )
