b245fce1467ffbd826dffc15beb00df2d052e46c
HenriqueChepli <henriquechepli@gmail.com>
2025-07-05 16:16:38 -0300

    feat: limit one cart per user and increment item quantity in PedidoCreateUpdateSerializer #27

--- core/serializers/pedido/pedido.py
+++ core/serializers/pedido/pedido.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer, CharField, SerializerMet
 from core.models.pedido.pedido import Pedido
 from core.models.pedido.item_pedido import ItemPedido
 from core.serializers.pedido.item_pedido import ItemPedidoSerializer, ItemPedidoCreateUpdateSerializer, ItemPedidoListSerializer
+from django.db import transaction
 
 class PedidoSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
@@ -17,6 +18,7 @@ class PedidoSerializer(ModelSerializer):
         model = Pedido
         fields = ('id', 'usuario', 'status', 'total', 'itens', 'data_de_retirada',)
 
+
 class PedidoCreateUpdateSerializer(ModelSerializer):
     usuario = HiddenField(default=CurrentUserDefault())
     itens = ItemPedidoCreateUpdateSerializer(many=True)
@@ -25,14 +27,42 @@ class PedidoCreateUpdateSerializer(ModelSerializer):
         model = Pedido
         fields = ('usuario', 'itens')
 
+    @transaction.atomic
     def create(self, validated_data):
         itens = validated_data.pop('itens')
-        pedido = Pedido.objects.create(**validated_data)
+        usuario = validated_data['usuario']
+
+        pedido, criado = Pedido.objects.get_or_create(
+            usuario=usuario,
+            status=Pedido.StatusPedido.CARRINHO,
+            defaults=validated_data
+        )
+
         for item in itens:
-            item['preco'] = item['produto'].preco # nova linha
-            ItemPedido.objects.create(pedido=pedido, **item)
-        pedido.save()
+            item_existente = pedido.itens.filter(produto=item['produto']).first()
+
+            if item_existente:
+                item_existente.quantidade += item['quantidade']
+                item_existente.preco = item['produto'].preco
+                item_existente.save()
+            else:
+                item['preco'] = item['produto'].preco
+                ItemPedido.objects.create(pedido=pedido, **item)
+
         return pedido
+
+    @transaction.atomic
+    def update(self, pedido, validated_data):
+        itens = validated_data.pop('itens', [])
+
+        if itens:
+            pedido.itens.all().delete()
+            for item in itens:
+                item['preco'] = item['produto'].preco
+                ItemPedido.objects.create(pedido=pedido, **item)
+
+        return super().update(pedido, validated_data)
+
 
 class PedidoListSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
