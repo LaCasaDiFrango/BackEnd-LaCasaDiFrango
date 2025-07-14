26270d07faeb8491fc682f7cdcb0472956cf10cf
HenriqueChepli <henriquechepli@gmail.com>
2025-07-14 20:03:54 -0300

    fix: arrumando fields de ProdutoListSerializer #38

--- core/serializers/produto/produto.py
+++ core/serializers/produto/produto.py
@@ -10,7 +10,7 @@ class ProdutoSerializer(ModelSerializer):
 class ProdutoListSerializer(ModelSerializer):
     class Meta:
         model = Produto
-        fields = ("id", "nome", "preco")
+        fields = ("id", "nome", "preco", "categoria")
 
 class ProdutoRetrieveSerializer(ModelSerializer):
     class Meta:
