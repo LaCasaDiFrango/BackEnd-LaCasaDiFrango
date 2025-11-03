5d0526b163cf305e094e65d79dd565330abec686
HenriqueChepli <henriquechepli@gmail.com>
2025-11-03 16:48:18 -0300

    feat: add 'mais_ativos' action to UserViewSet to return top 10 users with most orders

--- core/views/usuario/user.py
+++ core/views/usuario/user.py
@@ -6,6 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils import timezone
 from datetime import timedelta
+from django.db.models import Count
 
 from core.permissions import IsAdminUser, IsOwnerOrAdmin
 from core.models.usuario.user import User
@@ -18,7 +19,6 @@ class UserViewSet(ModelViewSet):
     pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
-    # Filtros e pesquisa
     filterset_fields = ['is_active', 'groups__name']
     search_fields = ['name', 'email']
     ordering_fields = ['id', 'name', 'email', 'ultimo_pedido']
@@ -38,19 +38,11 @@ class UserViewSet(ModelViewSet):
 
     @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
     def me(self, request):
-        """Retorna os dados do próprio usuário"""
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-
     @action(detail=False, methods=['get'], permission_classes=[IsAdminUser])
     def ativos_inativos(self, request):
-        """
-        Retorna contagem de usuários ativos e inativos.
-        Usuário ativo: último pedido nos últimos 30 dias
-        Usuário inativo: último pedido há mais de 30 dias ou nunca fez pedido
-        """
         hoje = timezone.localtime(timezone.now())
         limite = hoje - timedelta(days=30)
 
@@ -69,7 +61,22 @@ class UserViewSet(ModelViewSet):
                 inativos += 1
 
         return Response({
-        "ativos": ativos,
-        "inativos": inativos
+            "ativos": ativos,
+            "inativos": inativos
         }, status=status.HTTP_200_OK)
 
+    @action(detail=False, methods=['get'], permission_classes=[IsAdminUser])
+    def mais_ativos(self, request):
+        """
+        Retorna os top 10 usuários com mais pedidos
+        """
+        usuarios = (
+            User.objects
+            .annotate(total_pedidos=Count('pedidos'))  # 'pedidos' vem do related_name do Pedido
+            .order_by('-total_pedidos')[:10]
+        )
+        data = [
+            {'id': u.id, 'name': u.name, 'total_pedidos': u.total_pedidos}
+            for u in usuarios
+        ]
+        return Response(data, status=status.HTTP_200_OK)
